b7da26a76d110a34ca13b9df843fb60e9881d408
IMake <moooooooooooogle@gmail.com>
2015-03-18 10:28:07 +0700

    aoe

--- src/th/co/aoe/Chatchai.java
+++ src/th/co/aoe/Chatchai.java
@@ -3,6 +3,6 @@ package th.co.aoe;
 public class Chatchai {
 	public static void main(String[] args) {
 		System.out.println("asdfsd");
-		System.out.println("sdafsdddd Chatchai");
+		System.out.println("Chatchai");
 	}
 }
